d2c971c22dcca2495ae6f781336b3b42112588fc
bhaktiDemon <bhaktipvk24@gmail.com>
2025-11-21 01:25:02 +0000

    Update mainClass.java

--- src/test/java/Main/mainClass.java
+++ src/test/java/Main/mainClass.java
@@ -6,16 +6,16 @@ public class mainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc  = new Scanner(System.in);
-		
-		String a = sc.next();
-		String b = sc.next();
-		
-		int num1 = Integer.parseInt(a);
-		int num2 = Integer.parseInt(b);
-		
-		addition.addition(num1, num2);
-
+		try {
+        // Convert string arguments to integers
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+ 
+        // Call addition method
+        addition.addition(num1, num2);
+	    } catch (NumberFormatException e) {
+	        System.out.println("Error: Arguments must be valid integers.");
+	    }
 	}
 
 }
